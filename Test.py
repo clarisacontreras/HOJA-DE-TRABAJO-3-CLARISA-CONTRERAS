1a7b67f7a53d8e66ff5e726f9bdc56a7d5512128
Clarisa Contreras <clarisacontreras@ufm.edu>
2022-05-10 13:40:04 -0600

    Create Test.py

---
+++ Test.py
@@ -0,0 +1,110 @@
+''
+Testing of algorithms
+'''
+
+
+'''
+from sorting.bubble_sort import bubble_sort
+
+print("***Bubble Sort***")
+print("input: " , [3, 6, 6, 6, 30, 100, -50])
+print("output: " , bubble_sort([3, 6, 6, 6, 30, 100, -50]))
+'''
+
+'''
+from sorting.selection import SelectionSort
+
+print("***Selection Sort***")
+print("input: " , [3, 6, 6, 6, 30, 100, -50])
+print("output: " , SelectionSort([3, 6, 6, 6, 30, 100, -50]))
+'''
+'''
+from sorting.bubble_opt import bubble_opt
+
+print("***Bubble Sort Optimizado***")
+print("input: " , [3, 6, 6, 6, 30, 100, -50])
+print("output: " , bubble_opt([3, 6, 6, 6, 30, 100, -50]))
+'''
+'''
+from sorting.insertion import insertion
+
+print("***Insertion Sort***")
+print("input: " , [3, 6, 6, 6, 30, 100, -50])
+print("output: " , insertion([3, 6, 6, 6, 30, 100, -50]))
+'''
+'''
+from unittest import result
+import binary_search
+
+print("***Binary Search***")
+
+print("input: " , [3, 6, 6, 6, 30, 100, -50])
+print("output: " , binary_search([3, 6, 6, 6,] 30, 100, -50))
+
+if result == 1:
+    print("El elemento no fue encontrado")
+else:
+    print("El elemento está en la casilla: ", str(result))
+'''
+'''
+from searching.linear_search import linear_search
+
+print("***Linear Search***")
+print("input: " , [3, 6, 6, 6, 30, 100, -50])
+print("output: " , linear_search([3, 6, 6, 6], 30, 100, -50))
+'''
+'''
+from recursion.factorial import fact
+
+print("***Factorial de n***")
+print("El factorial es: " , fact(5))
+'''
+'''
+from recursion.countdown import cuenta_regresiva
+
+print("***Cuenta regresiva***")
+print(cuenta_regresiva(5+1))
+'''
+'''
+from recursion.Fibonacci import fib
+
+print("***Fibonacci***")
+print(fib(2))
+'''
+'''
+from recursion.suma import sum_of_n
+
+print("***suma de los primeros n numeros***")
+print(sum(10))
+'''
+'''
+from brute_force.unlock import unlock
+
+print("***Pin unlock***")
+print(unlock("1001"))
+'''
+'''
+from brute_force.divisores import divisors
+
+print("***Divisores de n***")
+n = 421
+result = divisors(421)
+print("n: " + str(n) + " divisors: " + str(result))
+'''
+'''
+from brute_force.sum_n_numbers import suma
+
+print("***suma de los primeros n numeros***")
+n = int(input("ingrese un numero n: "))
+'''
+'''
+from lists.list_merge import combinar
+
+print("***List merge***")
+combinar()
+'''
+'''
+from lists.largest_number import maximo
+print("***largest number***")
+maximo()
+'''
